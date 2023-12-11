8e98f3b22f8b4c823e8c96081c1b1a7ee30996a8
Ishita Agarwal <90623408+ishitagithub@users.noreply.github.com>
2023-12-11 17:01:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,145 +1,49 @@
-import { BsFillShieldLockFill, BsTelephoneFill } from "react-icons/bs";
-import { CgSpinner } from "react-icons/cg";
+import { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import './App.css';
 
-import OtpInput from "otp-input-react";
-import { useState } from "react";
-import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css";
-import { auth } from "./firebase.config";
-import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-import { toast, Toaster } from "react-hot-toast";
-
-const App = () => {
-  const [otp, setOtp] = useState("");
-  const [ph, setPh] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [showOTP, setShowOTP] = useState(false);
-  const [user, setUser] = useState(null);
-
-  function onCaptchVerify() {
-    if (!window.recaptchaVerifier) {
-      window.recaptchaVerifier = new RecaptchaVerifier(
-        "recaptcha-container",
-        {
-          size: "invisible",
-          callback: (response) => {
-            onSignup();
-          },
-          "expired-callback": () => {},
-        },
-        auth
-      );
-    }
-  }
-
-  function onSignup() {
-    setLoading(true);
-    onCaptchVerify();
-
-    const appVerifier = window.recaptchaVerifier;
-
-    const formatPh = "+" + ph;
-
-    signInWithPhoneNumber(auth, formatPh, appVerifier)
-      .then((confirmationResult) => {
-        window.confirmationResult = confirmationResult;
-        setLoading(false);
-        setShowOTP(true);
-        toast.success("OTP sended successfully!");
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
-      });
-  }
-
-  function onOTPVerify() {
-    setLoading(true);
-    window.confirmationResult
-      .confirm(otp)
-      .then(async (res) => {
-        console.log(res);
-        setUser(res.user);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setLoading(false);
-      });
-  }
-
+function App() {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('')
+  //const [address, setAddress] = useState('');
+  const ALAN_Key = '923e0990515cefca5c7a72a75b453d522e956eca572e1d8b807a3e2338fdd0dc/stage'
+  useEffect(() => {
+    alanBtn({
+        key: ALAN_Key,
+        onCommand: (commandData) => {
+          if(commandData.command === 'name'){
+            setName(commandData.data)
+          }
+          if(commandData.command === 'phone'){
+            setPhone(commandData.data)
+          }
+          /*if(commandData.command === 'address'){
+            setAddress(commandData.data)
+          }*/
+        }
+    });
+  }, []);
   return (
-    <section className="bg-emerald-500 flex items-center justify-center h-screen">
-      <div>
-        <Toaster toastOptions={{ duration: 4000 }} />
-        <div id="recaptcha-container"></div>
-        {user ? (
-          <h2 className="text-center text-white font-medium text-2xl">
-            👍Login Success
-          </h2>
-        ) : (
-          <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
-            <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6">
-              Welcome to <br /> CODE A PROGRAM
-            </h1>
-            {showOTP ? (
-              <>
-                <div className="bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full">
-                  <BsFillShieldLockFill size={30} />
-                </div>
-                <label
-                  htmlFor="otp"
-                  className="font-bold text-xl text-white text-center"
-                >
-                  Enter your OTP
-                </label>
-                <OtpInput
-                  value={otp}
-                  onChange={setOtp}
-                  OTPLength={6}
-                  otpType="number"
-                  disabled={false}
-                  autoFocus
-                  className="opt-container "
-                ></OtpInput>
-                <button
-                  onClick={onOTPVerify}
-                  className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-white rounded"
-                >
-                  {loading && (
-                    <CgSpinner size={20} className="mt-1 animate-spin" />
-                  )}
-                  <span>Verify OTP</span>
-                </button>
-              </>
-            ) : (
-              <>
-                <div className="bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full">
-                  <BsTelephoneFill size={30} />
-                </div>
-                <label
-                  htmlFor=""
-                  className="font-bold text-xl text-white text-center"
-                >
-                  Verify your phone number
-                </label>
-                <PhoneInput country={"in"} value={ph} onChange={setPh} />
-                <button
-                  onClick={onSignup}
-                  className="bg-emerald-600 w-full flex gap-1 items-center justify-center py-2.5 text-white rounded"
-                >
-                  {loading && (
-                    <CgSpinner size={20} className="mt-1 animate-spin" />
-                  )}
-                  <span>Send code via SMS</span>
-                </button>
-              </>
-            )}
-          </div>
-        )}
-      </div>
-    </section>
+    <div className='form-fields'>
+      
+      <input
+        placeholder='Phone'
+        name='phone'
+        onChange={(event) => setPhone(event.target.value)}
+        value={phone}
+        type='text'
+      />
+      <input
+        placeholder='Name'
+        name='name'
+        onChange={(event) => setName(event.target.value)}
+        value={name}
+        type='text'
+      />
+
+      <button className='submit-btn'>Submit</button>
+    </div>
   );
-};
+}
 
 export default App;
